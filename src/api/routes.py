b294155c1dd4d00bb5fad947658465350c944633
Gabriela Zambrano Rodríguez <102486455+gabrielazaro@users.noreply.github.com>
2023-06-04 09:25:48 +0000

    changes sandra

--- src/api/routes.py
+++ src/api/routes.py
@@ -9,46 +9,29 @@ from api.utils import generate_sitemap, APIException
 api = Blueprint('api', __name__)
 
 
-# @api.route('/hello', methods=['POST', 'GET'])
-# def handle_hello():
+@api.route('/hello', methods=['POST', 'GET'])
+def handle_hello():
 
-#     response_body = {
-#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-#     }
-
-#     return jsonify(response_body), 200
-
-@api.route('/sign-up', methods =['POST'])
-def sign_up():
-    data = request.json
-    print(data)
-    email = data.get("email")
-    password = data.get("password")
-    new_user = User(email=email, password=password)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({"msg": "Usuario agregado existosamente"}), 200
-
-@api.route('/login', methods=['POST'])
-def login():
-    # Obtener los datos del usuario desde el cliente
-    body = request.get_json()
-    email = body['email']
-    password = body['password']
-
-   
-    user = User.query.filter_by(email=email, password=password).first()
-
-    # si no existe, devuelve un mensaje de error y el código 401
-    if user == None:
-        return jsonify({"msg": "User or password, Not exist!"}), 401
-
-    # Flask crea un nuevo token JWT. Se lo guarda en su base de datos y lo asocia al usuario que hemos recuperado de la base de datos
-    access_token = create_access_token(identity=user.serialize())
-
-  
     response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+        "message": "Yeah baby!!! I'm a message that came from the funky backend, check the network tab on the google inspector and you will see the GET request"
     }
 
     return jsonify(response_body), 200
+
+# [GET] /users Listar todos los usuarios del blog
+@api.route('/user', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    all_users = list(map(lambda x: x.serialize(), users))
+    return jsonify(all_users), 200
+
+# post user
+@api.route('/signup', methods=['POST'])
+def add_users():
+
+    request_body_user = request.get_json()
+    new_user = User(
+        email=request_body_user['email'], password=request_body_user['password'])
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify('user added:', request_body_user), 200
