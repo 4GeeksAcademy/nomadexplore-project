e46654a95660325a2339843a98970dfc15ab980b
Gabriela Zambrano Rodríguez <102486455+gabrielazaro@users.noreply.github.com>
2023-06-04 09:13:48 +0000

    agregados

--- src/api/models.py
+++ src/api/models.py
@@ -2,39 +2,4 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class User(db.Model):
-    __tablename__ = 'user'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-
-    def __repr__(self):
-        return f'<User {self.email}>'
-
-    def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
-    
-class Destin(db.Model):
-    __tablename__ = 'destin'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return f'<Destin {self.name}>'
-    
-    
-class Favorites(db.Model):
-    __tablename__ = 'favorites'
-    id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    user = db.relationship('User', backref='favorites')
-    destin = db.relationship('Destin', backref='favorites')
-
-    def __repr__(self):
-        return f"Favorite(id={self.id}, user_id={self.user_id})"
 
