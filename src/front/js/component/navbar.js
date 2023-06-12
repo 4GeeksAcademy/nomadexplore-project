ef379b00a07dfa016e9334d775076bad7a052a52
Nico <nicosis@gmail.com>
2023-06-12 06:52:57 +0000

    navbar responsive

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -45,44 +45,97 @@ export const Navbar = () => {
 
 
 	return (
-		<nav className="navbar">
-			<div className="navbar-container">
+
+		<nav className="navbar navbar-dark bg-dark navbar-expand-lg navbar-transparent">
+			<div className="container-fluid">
 				<Link to="/" className="navbar-brand">
-					<span className="brand-text">NOMAD EXPLORE</span>
+					NOMAD EXPLORE
 				</Link>
-				<button className="social-buttons">
-					<i className="fa-brands fa-instagram"></i>
-					<a href="https://twitter.com/nomadexplore" style={{ color: "white" }} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-twitter"></i>
-					</a>
-					<i className="fa-brands fa-facebook"></i>
+				<button
+					className="navbar-toggler"
+					type="button"
+					data-bs-toggle="collapse"
+					data-bs-target="#navbarNavAltMarkup"
+					aria-controls="navbarNavAltMarkup"
+					aria-expanded="false"
+					aria-label="Toggle navigation"
+				>
+					<span className="navbar-toggler-icon" />
 				</button>
-				<div className="navbar-buttons-container">
-					<Link to="/aboutus" className="navbar-link">
-						About Us
-					</Link>
-					{tokenExists && (
-						<>
-							<Link to="/selection" className="navbar-link">
-								Select Destination
-							</Link>
-							<Link to="/planner" className="navbar-link">
-								Planner
-							</Link>
-							<br />
-							<button onClick={handleLogout}>
-								Hello: {loggedUserName} {"->"} Logout
-							</button>
-						</>
-					)}
-					{!tokenExists && (
-						<>
-							<Link to="/login">
-								<button className="navbar-link">Log In</button>
-							</Link>
-						</>
-					)}
+				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+					<div className="navbar-nav ms-auto">
+						<Link to="/" className="nav-link">
+							Social Media
+						</Link>
+						<Link to="/" className="nav-link">
+							About Us
+						</Link>
+						{tokenExists && (
+							<>
+								<Link to="/selection" className="nav-link">
+									Selection
+								</Link>
+								<Link to="/listFavs" className="nav-link">
+									Planner
+								</Link>
+								<a className="nav-link" href="#" onClick={handleLogout}>
+									Hello: {loggedUserName}- Logout
+								</a>
+							</>
+						)}
+						{!tokenExists && (
+							<>
+								<Link to="/login" className="nav-link">
+									Login
+								</Link>
+								<Link to="/signup" className="nav-link">
+									Signup
+								</Link>
+							</>
+						)}
+					</div>
 				</div>
 			</div>
 		</nav>
+
+		// <nav className="navbar">
+		// 	<div className="navbar-container">
+		// 		<Link to="/" className="navbar-brand">
+		// 			<span className="brand-text">NOMAD EXPLORE</span>
+		// 		</Link>
+		// 		<button className="social-buttons">
+		// 			<i className="fa-brands fa-instagram"></i>
+		// 			<a href="https://twitter.com/nomadexplore" style={{ color: "white" }} target="_blank" rel="noopener noreferrer"><i className="fa-brands fa-twitter"></i>
+		// 			</a>
+		// 			<i className="fa-brands fa-facebook"></i>
+		// 		</button>
+		// 		<div className="navbar-buttons-container">
+		// 			<Link to="/aboutus" className="navbar-link">
+		// 				About Us
+		// 			</Link>
+		// 			{tokenExists && (
+		// 				<>
+		// 					<Link to="/selection" className="navbar-link">
+		// 						Select Destination
+		// 					</Link>
+		// 					<Link to="/planner" className="navbar-link">
+		// 						Planner
+		// 					</Link>
+		// 					<br />
+		// 					<button onClick={handleLogout}>
+		// 						Hello: {loggedUserName} {"->"} Logout
+		// 					</button>
+		// 				</>
+		// 			)}
+		// 			{!tokenExists && (
+		// 				<>
+		// 					<Link to="/login">
+		// 						<button className="navbar-link">Log In</button>
+		// 					</Link>
+		// 				</>
+		// 			)}
+		// 		</div>
+		// 	</div>
+		// </nav>
 	);
 };
