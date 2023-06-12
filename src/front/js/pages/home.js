beedb7858f1727742e3350d89df7785c8e1a1e53
Gabriela Zambrano Rodríguez <102486455+gabrielazaro@users.noreply.github.com>
2023-06-12 19:29:32 +0000

    carousel added

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -20,10 +20,31 @@ export const Home = () => {
 					</div>
 				</div>
 			</div>
-			<div className="card-image-container"></div>
-			<div className="card">
-				<img src="https://media.istockphoto.com/id/682070916/es/foto/trabajador-balineso-del-campo-del-arroz-en-el-campo-del-arroz.jpg?s=612x612&w=0&k=20&c=P-1lOXsYyMfHKsEH4_wnKVisrEvDEhlznEeENaO0Tro=" alt="Card Image" />
+			<div className="carousel slide" id="carouselSlider">
+				<div className="carousel-inner">
+					<div className="carousel-item active">
+						<img src="https://media.istockphoto.com/id/682070916/es/foto/trabajador-balineso-del-campo-del-arroz-en-el-campo-del-arroz.jpg?s=612x612&w=0&k=20&c=P-1lOXsYyMfHKsEH4_wnKVisrEvDEhlznEeENaO0Tro=" alt="Card Image" className="w-100 h-100"/>
+					</div>
+					<div className="carousel-item">
+						<img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="Card Image" />
+					</div>
+					<div className="carousel-item">
+						<img src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="Card Image" />
+					</div>
+					<button className="carousel-control-prev" type="button" data-bs-target="#carouselSlider" data-bs-slide="prev">
+						<span className="carousel-control-prev-icon" ></span>
+						<span className="visually-hidden">Previous</span>
+					</button>
+					<button className="carousel-control-next" type="button" data-bs-target="#carouselSlider" data-bs-slide="next">
+						<span className="carousel-control-next-icon" ></span>
+						<span className="visually-hidden">Next</span>
+					</button>
+				</div>
 			</div>
 		</div>
+
+
+
+
 	);
 };
