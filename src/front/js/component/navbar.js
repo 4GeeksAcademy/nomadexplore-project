9ebb7fcbf9ae527d4ba4d2424caacd7795f92cfc
Sandra Montero <122152633+sandraMA87@users.noreply.github.com>
2023-06-13 19:39:10 +0000

    navbar-final

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -41,10 +41,11 @@ export const Navbar = () => {
 
 	return (
 
-		<nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+		<nav className="navbar navbar-dark navbar-expand-lg">
 			<div className="container-fluid">
 				<Link to="/" className="navbar-brand">
 					NOMAD EXPLORE
+					<i className="fa-solid fa-earth-americas"></i>
 				</Link>
 				<button
 					className="navbar-toggler"
@@ -59,28 +60,36 @@ export const Navbar = () => {
 				</button>
 				<div className="collapse navbar-collapse" id="navbarNavAltMarkup">
 					<div className="navbar-nav ms-auto">
-						<Link to="/" className="nav-link">
-							Social Media
-						</Link>
-						<Link to="/aboutus" className="nav-link">
+						<div className="navbar-icons text-white">
+							<a href="https://twitter.com/NomadExplore" target="_blank" rel="noopener noreferrer">
+								<i className="twitter-icon fa-brands fa-twitter text-white"></i>
+							</a>
+							<a href="https://twitter.com/NomadExplore" target="_blank" rel="noopener noreferrer">
+								<i className="instagram-icon fa-brands fa-instagram text-white"></i>
+							</a>
+							<a href="https://twitter.com/NomadExplore" target="_blank" rel="noopener noreferrer">
+								<i className="facebook-icon fa-brands fa-facebook text-white"></i>
+							</a>
+						</div>
+						<Link to="/aboutus" className="nav-link text-white">
 							About Us
 						</Link>
 						{tokenExists && (
 							<>
-								<Link to="/selection" className="nav-link">
+								<Link to="/selection" className="nav-link text-white">
 									Selection
 								</Link>
-								<Link to="/planner" className="nav-link">
+								<Link to="/planner" className="nav-link text-white">
 									Plan your trip
 								</Link>
-								<a className="nav-link" href="#" onClick={handleLogout}>
-									Hello: {loggedUserName} {"->"} Logout
+								<a className="nav-link text-white" href="#" onClick={handleLogout}>
+									<span className="mr-2">Hi {loggedUserName}!</span> <i className="fa-solid fa-person-walking-arrow-right mt-1 mr-2"></i>
 								</a>
 							</>
 						)}
 						{!tokenExists && (
 							<>
-								<Link to="/login" className="nav-link">
+								<Link to="/login" className="nav-link text-white">
 									Login
 								</Link>
 							</>
