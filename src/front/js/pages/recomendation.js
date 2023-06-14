57acc4723099e3d0e0703c36c310e4c7cce11c58
Nico <nicosis@gmail.com>
2023-06-14 20:42:52 +0000

    rr

--- src/front/js/pages/recomendation.js
+++ src/front/js/pages/recomendation.js
@@ -2,19 +2,19 @@ import React, { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 import destinationWeights from '../data/destinations.json';
-import "./recomendation.css";
 
 export const Recomendation = () => {
   const { store, actions } = useContext(Context);
   const [recommendedDestination, setRecommendedDestination] = useState(null);
   const [recommendedDescription, setRecommendedDescription] = useState(null);
   const [recommendedApiID, setRecommendedApiID] = useState(null);
+  const [recommendedImage, setRecommendedImage] = useState(null);
   const [alertVariant, setAlertVariant] = useState("");
   const [alertMessage, setAlertMessage] = useState("");
   const [buttonClicked, setButtonClicked] = useState(false);
-  const [tempMain, setTempMain] = useState(null);
-  const [weatherIconId, setWeatherIconId] = useState(null);
-  const [weatherDescription, setWeatherDescription] = useState(null);
+  const [tempMain, setTempMain] = useState("");
+  const [weatherIconId, setWeatherIconId] = useState("");
+  const [weatherDescription, setWeatherDescription] = useState("");
 
   const navigate = useNavigate();
 
@@ -23,61 +23,46 @@ export const Recomendation = () => {
     let recommendedDestination = null;
     let recommendedDescription = null;
     let recommendedApiID = null;
+    let recommendedImage = null;
 
     for (const i in destinationWeights) {
       const destinationWeight = destinationWeights[i];
-      const { destination, description, weights, apiID } = destinationWeight;
+      const { destination, description, weights, apiID, imageUrl } = destinationWeight;
       let score = 0;
 
       for (const category in store.userSelections) {
         score += store.userSelections[category] * weights[category];
       }
-
-      console.log(`Destination: ${destination}, Score: ${score}`); // Print destination and score in the console
+      console.log(destination + ":" + score);
 
       if (score > maxScore) {
         maxScore = score;
         recommendedDestination = destination;
         recommendedDescription = description;
         recommendedApiID = apiID;
+        recommendedImage = imageUrl;
       }
     }
     setRecommendedDestination(recommendedDestination);
     setRecommendedDescription(recommendedDescription);
     setRecommendedApiID(recommendedApiID);
-    console.log("Recommended Destination:", recommendedDestination); // Print recommended destination in the console
+    setRecommendedImage(recommendedImage);
   };
 
   useEffect(() => {
     calculateRecommendation();
   }, []);
 
-  useEffect(() => {
-    if (recommendedApiID) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?id=${recommendedApiID}&appid=0dc5a4cf75951d34619605b76e3b6f73`)
-        .then((res) => res.json())
-        .then((result) => {
-          const { main, weather } = result;
-          const minTemperature = main.temp_min;
-          const maxTemperature = main.temp_max;
-          const weatherDescription = weather[0].main;
-          console.log("Minimum Temperature:", minTemperature);
-          console.log("Maximum Temperature:", maxTemperature);
-          console.log("Weather Description:", weatherDescription);
-          setTempMain(main);
-          setWeatherIconId(weather[0].icon);
-          setWeatherDescription(weatherDescription);
-        });
-    }
-  }, [recommendedApiID]);
-
   const handleAddFav = async () => {
+
     const token = localStorage.getItem("miTokenJWT");
 
     if (!token) {
-     
+      // Mmmmm... no tengo el token, no debería poder acceder a está página de React
       navigate('/login');
     }
+
+
 
     const response = await fetch(process.env.BACKEND_URL + "/api/favs", {
       method: "POST",
@@ -92,65 +77,80 @@ export const Recomendation = () => {
 
     if (response.ok) {
       setAlertVariant("success");
-      setAlertMessage(data.msg);
+      setAlertMessage("Favorito añadido correctamente");
+      setButtonClicked(true);
     } else {
       setAlertVariant("danger");
-      setAlertMessage(data.msg);
+      setAlertMessage(data.error || "Error al añadir el favorito. Mira la consola o en el terminal del servidor de Python");
+      setButtonClicked(true);
     }
-
-    setButtonClicked(true);
   };
 
+  const apiUrl = "https://api.openweathermap.org/data/2.5/weather?id="
+  const apiKey = "&appid=4793b8122ea405851f5579246c1395fd&units=metric"
+  // const destinationApiId = 1726707
+  const iconUrl = "https://openweathermap.org/img/wn/"
+  const weatherIcon = iconUrl + weatherIconId + ".png"
+  console.log(weatherIcon);
+  console.log('id destino: ', recommendedApiID);
+
+  useEffect(() => {
+    if (recommendedApiID) {
+      fetch(apiUrl + recommendedApiID + apiKey)
+        .then((response) => response.json())
+        .then((data) => {
+          setTempMain(data.main);
+          setWeatherIconId(data.weather[0].icon);
+          setWeatherDescription(data.weather[0].description);
+          console.log('api clima', data.weather[0]);
+        })
+        .catch((error) => console.log(error));
+    }
+  }, [recommendedApiID]);
+
+
   return (
-
     <div>
-    <div className="container py-4">
-      <div className="p-5 mb-4 bg-light rounded-3 jumbotron">
-        <div className="container-fluid py-5">
-          <h1 className="display-5 fw-bold text-dark jumbotron-destination">{recommendedDestination}</h1>
-          <p className="col-md-8 fs-4 text-dark jumbotron-description">
-            {recommendedDescription}
-          </p>
-          {/*  <p className="jumbotron-description">{recommendedApiID}</p> */}
-          <div className="temperature-info">
-            {weatherIconId && (
-              <img
-                src={`https://openweathermap.org/img/wn/${weatherIconId}.png`}
-                alt="icon"
-              />
-            )}
-            {tempMain && (
-              <>
-                <p className="temperature-text">Min: {tempMain.temp_min}ºC</p>
-                <p className="temperature-text">Max: {tempMain.temp_max}ºC</p>
-                <p className="temperature-text">Humidity: {tempMain.humidity}%</p>
-              </>
-            )}
-          </div>
-
-          <div className="temperature-info mt-4">
-            <p className="display-6 fw-bold text-dark">{weatherDescription}</p>
-            {tempMain && (
-              <>
-                <p className="temperature-text">Temp: {tempMain.temp}ºC</p>
-                <p className="temperature-text">Feels like: {tempMain.feels_like}ºC</p>
-              </>
-            )}
-          </div>
-
-          <button className="btn btn-primary btn-lg add-fav-button" type="button" onClick={handleAddFav}>
-            Me gusta
-          </button>
-          {alertMessage && (
-            <div className={`alert jumbotron-alert alert-${alertVariant}`} role="alert">
-              {alertMessage}
+      <div className="container py-4">
+        <div className="p-5 mb-4 bg-light rounded-3">
+          <div className="container-fluid py-5">
+            <h1 className="display-5 fw-bold text-dark">{recommendedDestination}</h1>
+            <p className="col-md-8 fs-4 text-dark">{recommendedDescription}</p>
+            {/* <p className="col-md-8 fs-4 text-dark">{recommendedApiID}</p> */}
+            <div className="row">
+              <div className="col-md-3 d-flex justify-content-center">
+                <img style={{ width: "300px" }} src={recommendedImage} alt="image" />
+              </div>
+              <div className="col-md-6 d-flex justify-content-center align-items-center">
+                <div>
+                  <img style={{ width: '100px' }} src={weatherIcon} alt="icon" />
+                  <p className="display-5 fw-bold text-dark">Temp: {tempMain.temp}ºC</p>
+                  <p className="display-6 fw-bold text-dark">Feels like: {tempMain.feels_like}ºC</p>
+                </div>
+              </div>
+              <div className="col-md-3 d-flex justify-content-center align-items-center">
+                <div>
+                  <p className="display-6 fw-bold text-dark">{weatherDescription}</p>
+                  <p className="display-7 fw-bold text-dark">Min: {tempMain.temp_min}ºC</p>
+                  <p className="display-7 fw-bold text-dark">Max: {tempMain.temp_max}ºC</p>
+                  <p className="display-7 fw-bold text-dark">Humidity: {tempMain.humidity}%</p>
+                </div>
+              </div>
             </div>
-          )}
+            <button className="btn btn-primary btn-lg" type="button" onClick={handleAddFav}>
+              Me gusta
+            </button>
+            {alertMessage && (
+              <div className={`alert alert-${alertVariant}`} role="alert">
+                {alertMessage}
+              </div>
+            )}
+          </div>
         </div>
+        <footer className="pt-3 mt-4 text-muted border-top">© 2023</footer>
       </div>
-      <footer className="pt-3 mt-4 text-muted border-top">© 2023</footer>
     </div>
-  </div>
-);
 
-       }  
+
+  );
+};
