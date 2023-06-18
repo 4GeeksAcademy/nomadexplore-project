08c9cf91a08ac46110051b6161472331062ae7b9
Gabriela Zambrano Rodríguez <102486455+gabrielazaro@users.noreply.github.com>
2023-06-18 18:30:25 +0000

    imagenes actualizadas

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -3,7 +3,7 @@ import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
 import Palm from "../../img/palm.jpeg";
 import "../../styles/home.css";
-import imageUrl from '../data/destinations.json';
+
 
 
 export const Home = () => {
@@ -24,13 +24,25 @@ export const Home = () => {
 					<div className="carousel slide carousel-container" id="carouselSlider">
 						<div className="carousel-inner">
 							<div className="carousel-item active">
-								<img src={imageUrl[1]} alt="Card Image" className="d-block w-100 h-100"/>
+							<img src="https://images.unsplash.com/photo-1601836743857-4d1e6da20a32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1632&q=80" alt="Card Image" className="d-block w-100 h-100"/>
 							</div>
 							<div className="carousel-item">
-								<img src="https://images.unsplash.com/photo-1508138221679-760a23a2285b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80" alt="Card Image" className="d-block w-100 h-100"/>
+								<img src="https://images.unsplash.com/photo-1632660967293-ec569f27dfd0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=978&q=80" alt="Card Image" className="d-block w-100 h-100"/>
 							</div>
 							<div className="carousel-item ">
-								<img src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1470&q=80" alt="Card Image" className="d-block w-100 h-100"/>
+								<img src="https://images.unsplash.com/photo-1551641506-ee5bf4cb45f1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1184&q=80" alt="Card Image" className="d-block w-100 h-100"/>
+							</div>
+							<div className="carousel-item ">
+								<img src="https://images.unsplash.com/photo-1630219694734-fe47ab76b15e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1052&q=80" alt="Card Image" className="d-block w-100 h-100"/>
+							</div>
+							<div className="carousel-item ">
+								<img src="https://images.unsplash.com/photo-1550850839-8dc894ed385a?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1175&q=80" alt="Card Image" className="d-block w-100 h-100"/>
+							</div>
+							<div className="carousel-item ">
+								<img src="https://images.unsplash.com/photo-1518638150340-f706e86654de?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1167&q=80" alt="Card Image" className="d-block w-100 h-100"/>
+							</div>
+							<div className="carousel-item ">
+								<img src="https://images.unsplash.com/photo-1552751753-0fc84ae5b6c8?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80" alt="Card Image" className="d-block w-100 h-100"/>
 							</div>
 							<button className="carousel-control-prev" type="button" data-bs-target="#carouselSlider" data-bs-slide="prev">
 								<span className="carousel-control-prev-icon" ></span>
@@ -47,4 +59,3 @@ export const Home = () => {
 		</div>
 	);
 };
-
